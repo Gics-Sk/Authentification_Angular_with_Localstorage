1fe54d3f7c3677d020d69edf2ff5db08a61c79d0
Giovanni <giovanni.sokenou2105@gmail.com>
2024-08-14 21:06:21 +0200

    ok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
+import { FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +7,30 @@ import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
- 
+  loginForm: FormGroup;
+  storageData: any;
+password !: string;
+user !: string;
+constructor(private formBuilder:FormBuilder){
+this.loginForm = this.formBuilder.group({
+  user :['',Validators.required],
+  password : ['',[
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+  ]]
+});
+}
+onSubmit(user:string,password:string){
+localStorage.setItem("User",user);
+localStorage.setItem("Password",password);
+console.log(localStorage.getItem("User"));
+
+    
+//   if(this.loginForm.valid){
+//  return this.loginForm.value;
+//   }else{
+//     console.log('Formulaire non soumis');
+//   }
+}
 }
--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-verification',
   templateUrl: './verification.component.html',
   styleUrl: './verification.component.css'
 })
 export class VerificationComponent {
+ 
+
 
 }
